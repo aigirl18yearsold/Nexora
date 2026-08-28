54d280b5b4b1629eff8b8b5cfdf4ce7cc45257b4
Homyra Akther shaila <khatunshaila02@gmail.com>
2026-08-28 22:32:44 +0600

    Initialize Firebase in main function
    Updated main function to initialize Firebase before running the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'screens/splash_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyAyv4UOfqCUsFFDCA5Uj2sdCQQrz40euVg',
+      authDomain: 'nexora-c5988.firebaseapp.com',
+      projectId: 'nexora-c5988',
+      storageBucket: 'nexora-c5988.firebasestorage.app',
+      messagingSenderId: '563694175364',
+      appId: '1:563694175364:web:d277689d418d1422616ed1',
+      measurementId: 'G-TR142RBBS2',
+    ),
+  );
 
   runApp(const Nexora());
 }
@@ -15,22 +28,14 @@ class Nexora extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Nexora",
-
+      title: 'Nexora',
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
-
         colorSchemeSeed: Colors.indigo,
-
         useMaterial3: true,
-
         textTheme: GoogleFonts.poppinsTextTheme(),
-
       ),
-
       home: const SplashScreen(),
-
     );
   }
 }
