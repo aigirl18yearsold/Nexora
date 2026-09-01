2cb90956364e346c2cd8010c32685bf18645d403
Homyra Akther shaila <khatunshaila02@gmail.com>
2026-09-01 07:50:00 +0600

    Fix HomeScreen constructor usage in navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -54,7 +54,7 @@ class _LoginScreenState extends State<LoginScreen> {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-          builder: (context) => const HomeScreen(),
+          builder: (context) => HomeScreen(),,
         ),
       );
     } on FirebaseAuthException catch (e) {
